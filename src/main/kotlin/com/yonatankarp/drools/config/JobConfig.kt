21d501feee732e7459f24fb6dbdf162af085fc2c
javier <javier@andis.gob.ar>
2024-07-16 19:46:11 -0300

    Vemo

--- src/main/kotlin/com/yonatankarp/drools/config/JobConfig.kt
+++ src/main/kotlin/com/yonatankarp/drools/config/JobConfig.kt
@@ -1,40 +1,40 @@
 package com.yonatankarp.drools.config
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration
-import org.springframework.batch.core.repository.JobRepository
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean
-import org.springframework.batch.support.DatabaseType
+//import org.springframework.batch.core.repository.JobRepository
+//import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean
+//import org.springframework.batch.support.DatabaseType
 import org.springframework.context.annotation.Configuration
-import org.springframework.jdbc.datasource.DataSourceTransactionManager
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
+//import org.springframework.jdbc.datasource.DataSourceTransactionManager
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
 import org.springframework.transaction.PlatformTransactionManager
-import org.springframework.transaction.TransactionManager
-import javax.sql.DataSource
+//import org.springframework.transaction.TransactionManager
+//import javax.sql.DataSource
 
 
 @Configuration
 //@EnableBatchProcessing
-class JobConfig(private val transactionManager: DataSourceTransactionManager) : DefaultBatchConfiguration() {
+class JobConfig(/*private val transactionManager: DataSourceTransactionManager*/) : DefaultBatchConfiguration() {
 
-    @Throws(Exception::class)
+    /*@Throws(Exception::class)
     protected fun createJobRepository(): JobRepository {
         val factory = JobRepositoryFactoryBean()
         factory.setDatabaseType(DatabaseType.H2.productName)
-        factory.setDataSource(dataSource());
+        factory.setDataSource(dataSource())
         factory.transactionManager = getTransactionManager()
         return factory.getObject()
-    }
+    }*/
 
     fun transactionManager() : PlatformTransactionManager {
         return getTransactionManager()
     }
-    fun dataSource(): DataSource {
+ /*   fun dataSource(): DataSource {
         val embeddedDatabaseBuilder = EmbeddedDatabaseBuilder()
         return embeddedDatabaseBuilder
             .addScript("classpath:org/springframework/batch/core/schema-drop-h2.sql")
             .addScript("classpath:org/springframework/batch/core/schema-h2.sql")
             .setType(EmbeddedDatabaseType.H2).build()
-    }
+    }*/
 }
